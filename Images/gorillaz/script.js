33306c0ab9ad64d73481b259d3390782d6c0d4e8
sguillen2 <45013630+sguillen2@users.noreply.github.com>
2019-12-06 15:13:42 -0800

    Add files via upload

---
+++ Images/gorillaz/script.js
@@ -0,0 +1,28 @@
+// use variables to simplify your life!
+var t = document.getElementById("truck");
+var g = document.getElementById("gorillaz");
+
+
+// functions for the faces
+function changeImage1(){
+	document.getElementById("img1").src = "images/murdoc.gif";
+}
+
+
+
+
+
+
+// function for the truck
+function engineStart(){
+
+}
+
+// functions for UX when user clicks on images
+function grayScaleGone(el) {
+
+}
+
+function noOpaque(el) {
+
+}
